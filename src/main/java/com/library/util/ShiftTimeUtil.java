93e675ea5518f2517c9683e0326ffc3f44951a17
Afsar Hussain <logicsoft152@gmail.com>
2026-07-22 11:40:54 +0530

    Change the toMinutes function

--- src/main/java/com/library/util/ShiftTimeUtil.java
+++ src/main/java/com/library/util/ShiftTimeUtil.java
@@ -22,11 +22,23 @@ public class ShiftTimeUtil {
 
 	private static int toMinutes(String time) {
 		String[] tokens = time.trim().split(" ");
-		int hour = Integer.parseInt(tokens[0]);
+		String hourPart = tokens[0];                 // "8:00" or "8"
 		String period = tokens.length > 1 ? tokens[1] : "";
+
+		int hour;
+		int minute = 0;
+		if (hourPart.contains(":")) {
+			String[] hm = hourPart.split(":");
+			hour = Integer.parseInt(hm[0]);
+			minute = Integer.parseInt(hm[1]);
+		} else {
+			hour = Integer.parseInt(hourPart);
+		}
+
 		if ("PM".equalsIgnoreCase(period) && hour != 12) hour += 12;
 		if ("AM".equalsIgnoreCase(period) && hour == 12) hour = 0;
-		return hour * 60;
+
+		return hour * 60 + minute;
 	}
 
 	public static boolean isOverlap(Range a, Range b) {
