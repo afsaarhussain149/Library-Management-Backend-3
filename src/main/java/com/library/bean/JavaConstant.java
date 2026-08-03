918c1d81af0aad5d81aeeccd98c03cfc7339e937
Afsar Hussain <logicsoft152@gmail.com>
2026-08-03 15:28:36 +0530

    update the payment query

--- src/main/java/com/library/bean/JavaConstant.java
+++ src/main/java/com/library/bean/JavaConstant.java
@@ -48,8 +48,9 @@ public class JavaConstant {
 	public final static String UPDATE_PAYMENT_ACTIVE_STATUS =
 			"update payment set is_active = ?1, updated_at = CURRENT_TIMESTAMP where payment_id = ?2";
 	public final static String UPDATE_PAYMENT_DEACTIVATE_AND_EXPIRE =
-		    "update payment set is_active = false, seats = null, plan_expire_seat_block = true, " +
-		    "end_plan_date = CURRENT_DATE, updated_at = CURRENT_TIMESTAMP where payment_id = ?1";
+	        "update payment set is_active = false, seats = null, shift_label = null, shift_time = null, " +
+	        "plan_hours = null, plan_expire_seat_block = true, " +
+	        "end_plan_date = CURRENT_DATE, updated_at = CURRENT_TIMESTAMP where payment_id = ?1";
 	public final static String GET_PAYMENT_BY_ORDER_ID =
 			"select * from payment where razorpay_order_id = ?1";
 	public final static String GET_PAYMENT_BY_ID =
